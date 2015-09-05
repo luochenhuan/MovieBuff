c9319ac5a45ede7dbc6dc2e758233879ee59a955
luochenhuan <zhenhaiyucelia@gmail.com>
2015-09-05 15:43:36 -0400

    create values-v21; MainActivity extends FragmentActivity; before change frame layout

--- PopularMovie/app/src/main/java/com/example/zhenhaiyu/popularmovie/MainActivity.java
+++ PopularMovie/app/src/main/java/com/example/zhenhaiyu/popularmovie/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.zhenhaiyu.popularmovie;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
